37706ee912c5c93d10dd3c1c537b64d8ec65d253
TugrulhanCinar <tugrulhan.cinar@volantx.com>
2022-05-24 14:20:00 +0300

    Add state example

--- swiftui-learning/ContentView.swift
+++ swiftui-learning/ContentView.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 
+struct IconFieldView: View {
+
+    var iconName: String = "Hello"
+    @State var textFieldValue: String = ""
+
+    var body: some View {
+        VStack {
+            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $textFieldValue)
+            Text(textFieldValue)
+                .font(.largeTitle)
+            Image(systemName: textFieldValue.lowercased()).frame(width: 100, height: 100, alignment: .center)
+        }
+    }
+}
+
 struct ContentView: View {
 
     var body: some View {
@@ -104,6 +119,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        IconFieldView()
     }
 }
