23e72e46b5229eaa3d026674e3f2d46202476c2c
TugrulhanCinar <tugrulhan.cinar@volantx.com>
2022-05-24 11:59:50 +0300

    Add stack views

--- swiftui-learning/ContentView.swift
+++ swiftui-learning/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        sampleStackView()
+    }
+
+    fileprivate func sampleVerticalView() -> some View {
+        return HStack{
+            // Yatay yerleştiriyor.
+            Spacer().frame(width: 100, alignment: .center)
+            Text("tgrl").padding()
+            Text("tgrl").padding()
+            Text("tgrl").padding()
+            Color.red
+            Spacer().frame(width: 100, alignment: .center)
+        }
+    }
+
+    fileprivate func sampleHorizontalView() -> some View {
+        // Dikey yerleştiriyor.
+        return VStack{
+            Spacer().frame(width: 100, alignment: .center)
+            Text("tgrl").padding()
+            Text("tgrl").padding()
+            Text("tgrl").padding()
+            Color.red
+            Spacer().frame(width: 100, alignment: .center)
+        }
+    }
+
+    fileprivate func sampleStackView() -> some View {
+        // en üste koyduğun en alta gelecek. (üst üste)
+        return ZStack(alignment: .bottom) {
+            Spacer().frame(width: 100, alignment: .center)
+            Text("tgrl").padding()
+            Text("tgrl").padding()
+            Text("tgrl").padding()
+            Color.red.opacity(0.3)
+            Spacer().frame(width: 100, alignment: .center)
+        }
     }
 }
 
