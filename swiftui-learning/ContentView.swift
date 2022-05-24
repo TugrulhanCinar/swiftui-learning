07d2ffb925e1d8f841f9ccd04fe8093bee7e329f
TugrulhanCinar <tugrulhan.cinar@volantx.com>
2022-05-24 12:36:29 +0300

    Add new sample view

--- swiftui-learning/ContentView.swift
+++ swiftui-learning/ContentView.swift
@@ -10,7 +10,36 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        sampleStackView()
+        sampleView1()
+    }
+
+    fileprivate func sampleView1() -> some View {
+        // geçerli view ı seçip sağ menüden padding, color gibi özellikleri eklenebilir.
+        return VStack(){
+            Rectangle().frame(height: 100, alignment: .center)
+            Circle().foregroundColor(.red).frame(height: 100, alignment: .center)
+            Text("Sample1")
+            Text("Sample2")
+            Text("Sample3")
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            Text("Sample5")
+                .fontWeight(.light)
+                .foregroundColor(Color(hue: 0.529, saturation: 0.874, brightness: 0.661))
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+            Spacer(minLength: 10)
+            HStack{
+                RoundedRectangle(cornerRadius: 25).frame(height: 100)
+                Spacer(minLength: 100)
+                ZStack {
+                    Circle()
+                    Text("asfdasdf").foregroundColor(Color(hue: 0.583, saturation: 0.844, brightness: 0.798))
+                }.frame(height: 100.0)
+
+            }.frame(height: 500, alignment: .top)
+
+        }
     }
 
     fileprivate func sampleVerticalView() -> some View {
