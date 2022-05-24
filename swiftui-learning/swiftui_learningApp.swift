248c9d9ad25c5b6990b43a5a2858a280fcca0615
TugrulhanCinar <tugrulhan.cinar@volantx.com>
2022-05-24 10:20:54 +0300

    Initial Commit

---
+++ swiftui-learning/swiftui_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_learningApp.swift
+//  swiftui-learning
+//
+//  Created by Tugrulhan Çınar on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
