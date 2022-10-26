f914a69edcf2662f3b92bb7461920061ab83b7f2
TugrulhanCinar <tgrl_3@hotmail.com>
2022-10-26 11:44:30 +0300

    Add Snack Over Flow target

---
+++ SnackOverFlow/SnackOverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackOverFlowApp.swift
+//  SnackOverFlow
+//
+//  Created by Tuğrul on 26.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnackOverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
